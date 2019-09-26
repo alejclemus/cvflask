1a325fd503edfdc890a52288b45246d94c84584e
Ale <alejdelcomp@gmail.com>
2019-09-25 23:38:32 -0600

    Added routes

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ environment=os.getenv("ENVIRONMENT","development")
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
     return "Hello World!"         # which returns "hello world"
+
+@app.route("/information")
+def info():
+    return "test1"
+
+@app.route("/academics")
+def academics():
+    return "test2"
+
+@app.route("/experience")
+def experience():
+    return "test3"
+
 if __name__ == "__main__":        # on running python app.py
     debug=False
     if environment == "development" or environment == "local":
